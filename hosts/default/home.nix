cc60a5689db6d4ff0c2205231dd9dba7d3ecf523
Kevin Sheth <kevinsheth25@gmail.com>
2024-05-01 00:58:03 -0400

    tidy

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -29,8 +29,6 @@
   home.packages = with pkgs; [
     # utils
     bat
-    jq
-    eza
     git
     coreutils
     unzip
@@ -39,8 +37,6 @@
     ripgrep
     gcc
     curl
-    jq
-    tmux
     tree-sitter
     fd
     gnumake
@@ -69,39 +65,13 @@
     deadnix
     stylua
     lua52Packages.luacheck
-
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
     ".config/lvim/config.lua".source = ../../dotfiles/.config/lvim/config.lua;
     ".codeium/config.json".text = "{\"apiKey\":\"${secrets.codeium.api_key}\"}";
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   # Home Manager can also manage your environment variables through
@@ -154,10 +124,11 @@
           };
         }
       ];
+
       shellAliases = {
         cd = "z";
         bat = "cat";
-        nvim = "sudo lvim";
+        nvim = "lvim";
         refresh = "source ${config.home.homeDirectory}/.zshrc";
         rbd = "sudo nixos-rebuild switch --flake ~/configuration#default";
       };
@@ -196,9 +167,6 @@
           "https://oauth2:${secrets.github.oauth_token}@github.com" = {
             insteadOf = "https://github.com";
           };
-          #   "https://oauth2:${secrets.gitlab_token}@gitlab.com" = {
-          #     insteadOf = "https://gitlab.com";
-          #   };
         };
         push = {
           default = "current";
