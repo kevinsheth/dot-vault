1bf6bf6096c6046ef70c1311bce45be1a28f4100
Kevin Sheth <kevinsheth25@gmail.com>
2024-05-02 19:14:52 -0400

    clean up modules

--- dotfiles/nixvim/config/default.nix
+++ dotfiles/nixvim/config/default.nix
@@ -1,28 +1,8 @@
 {
   # Import all your configuration modules here
   imports = [
-    ./bufferline.nix
-    ./telescope.nix
-    ./treesitter.nix
-    ./nvim-tree.nix
-    #    ./codeium.nix
-    ./lsp.nix
-    ./cmp.nix
-    ./lazy.nix
-    ./friendly-snippets.nix
-    ./luasnip.nix
-    ./comment.nix
-    ./nvim-autopairs.nix
-    ./gitsigns.nix
-    ./which-key.nix
-    ./project-nvim.nix
-    ./lualine.nix
-    ./navic.nix
-    ./dap.nix
-    ./alpha.nix
-    #    ./toggleterm.nix
-    #    ./illuminate.nix
-    #    ./indent-blankline.nix
+    ./plugins.nix
+    ./keymaps.nix
   ];
 
   config = {
@@ -31,16 +11,6 @@
       mapleader = " ";
       maplocalleader = " ";
     };
-    keymaps = [
-      {
-        action = "<cmd>NvimTreeToggle><CR>";
-        key = "<C-n>";
-      }
-      {
-        action = "<cmd>Telescope find_files<CR>";
-        key = "<leader>ff";
-      }
-    ];
   };
 
   options = {
