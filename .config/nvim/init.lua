044c1158b9fc5160270d68dee1fa5fda8f2516e6
Kevin Sheth <kevinsheth25@gmail.com>
2024-11-05 10:19:24 -0500

    ocamllsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -378,8 +378,8 @@ require('lazy').setup({
         pyright = {},
         bashls = {},
         dockerls = {},
-        ruff = {},
-        kotlin_language_server = {},
+        ruff_lsp = {},
+        ocamllsp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
